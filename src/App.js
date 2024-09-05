e1ecce404221cb75c2d2251fc37d86ef46fb3fca
Daria Pavlova <dmpavlova@yandex.ru>
2024-09-05 14:50:17 +0300

    added Card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Card } from "./entities/Card/ui/Card";
 import { Header } from "./entities/Header/ui/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Card id='1' promotion='50' category='Chair' name='Wooden sofa chair' price='160.00' newPrice='80.00' rating='4.9'/>
     </div>
   );
 }
