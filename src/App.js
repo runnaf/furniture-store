d669bb3c803382b8a5463af4c2f97daaae948a4c
DariaDovydova <vip.dovydova@list.ru>
2024-09-11 17:45:33 +0300

    made layout of navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Card } from "./entities/Card/ui/Card";
 import { Header } from "./entities/Header/ui/Header";
 import Timer from "./entities/timer/ui/Timer";
+import Navbar from "./widgets/Navbar/ui/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Card id='1' promotion='50' category='Chair' name='Wooden sofa chair' price='160.00' newPrice='80.00' rating='4.9'/>
-      <Timer styleMode='timerCardContainer' endTime={'2024-12-31T23:59:59.000Z'}/>
-      <Timer styleMode='timerMainContainer' endTime={'2024-12-31T23:59:59.000Z'}/>
-      <Timer styleMode='timerComingSoonContainer' endTime={'2024-12-31T23:59:59.000Z'}/>
+      <Navbar />
+      <Card id='1' promotion='50' category='Chair' name='Wooden sofa chair' price='160.00' newPrice='80.00' rating='4.9' />
+      <Timer styleMode='timerCardContainer' endTime={'2024-12-31T23:59:59.000Z'} />
+      <Timer styleMode='timerMainContainer' endTime={'2024-12-31T23:59:59.000Z'} />
+      <Timer styleMode='timerComingSoonContainer' endTime={'2024-12-31T23:59:59.000Z'} />
     </div>
   );
 }
---
+++ src/widgets/Navbar/ui/Navbar.js
@@ -0,0 +1,37 @@
+import styles from './Navbar.module.scss';
+import { logoIcon, searchIcon, likeIcon, cartIcon, loginIcon } from '../../../shared/assets/svg/navbarIcons';
+import { Stack } from '../../../shared/ui/Stack/Stack';
+
+const Navbar = () => {
+    return (
+        <Stack justify="justifyAround" align="alignCenter" className={styles.navbar}>
+            <Stack>
+                {logoIcon()}
+            </Stack>
+            <Stack className={styles.navLinks}>
+                <a href="/">Home</a>
+                <a href="/shop">Shop</a>
+                <a href="/categories">Categories</a>
+                <a href="/about">About Us</a>
+                <a href="/contact">Contact Us</a>
+                <a href="/blog">Blog</a>
+            </Stack>
+            <Stack gap="24">
+                <button>
+                    {searchIcon()}
+                </button>
+                <button>
+                    {likeIcon()}
+                </button>
+                <button>
+                    {cartIcon()}
+                </button>
+                <button>
+                    {loginIcon()}
+                </button>
+            </Stack>
+        </Stack>
+    );
+};
+
+export default Navbar;
