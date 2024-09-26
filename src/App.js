e3f149d0499b3f944b8e38b79638865dc98c2d46
mrsBarul <dobro.ve@mail.ru>
2024-09-27 00:37:38 +0300

    flashSale,adaptation flashSale; adaptation mainTimer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Header } from "./entities/Header/ui/Header";
+import { Stack } from "./shared/ui/Stack/Stack";
+import { FlashSale } from "./widgets/FlashSale/ui/FlashSale";
 import { Navbar } from "./widgets/Navbar/ui/Navbar";
 
 function App() {
   return (
-    <>
+    <Stack direction="column"
+    align='alignCenter'>
       <Header />
       <Navbar/>
-    </>
+      <FlashSale/>
+    </Stack>
   );
 }
 
