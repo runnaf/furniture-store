1ba76c961b9e18b9407f6a4c666f0f91487a3b89
DariaDovydova <vip.dovydova@list.ru>
2024-10-02 15:41:20 +0300

    deleted react fragments in Slider and Signin

--- src/entities/Slider/ui/Slider.js
+++ src/entities/Slider/ui/Slider.js
@@ -2,9 +2,5 @@ import imageSigninPage from "../../../shared/assets/svg/imageSigninPage.svg"
 import styles from "./Slider.module.scss";
 
 export const Slider = () => {
-    return (
-        <>
-        <img src={imageSigninPage} className={styles.image} alt="signin" />
-        </>
-    )
+    return <img src={imageSigninPage} className={styles.image} alt="signin" />
 }
--- src/feature/Signin/ui/Signin.js
+++ src/feature/Signin/ui/Signin.js
@@ -1,9 +1,5 @@
 import { SigninForm } from "./SigninForm";
 
 export const Signin = () => {
-    return (
-        <>
-            <SigninForm />
-        </>
-    )
+    return <SigninForm />
 }
