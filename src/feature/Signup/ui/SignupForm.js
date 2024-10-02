3a6bf4af82a91cb1ca663d18c941ec59c2078c5a
DariaDovydova <vip.dovydova@list.ru>
2024-10-02 21:06:05 +0300

    made a layout of signup-page

--- src/feature/Signup/ui/SignupForm.js
+++ src/feature/Signup/ui/SignupForm.js
@@ -1,4 +1,4 @@
-import styles from './SigninForm.module.scss';
+import styles from './SignupForm.module.scss';
 import { Stack } from '../../../shared/ui/Stack/Stack';
 import { Text } from '../../../shared/ui/Text/Text';
 import { Input } from '../../../shared/ui/Input/Input';
@@ -9,7 +9,7 @@ import checkmarkIcon from '../../../shared/assets/svg/checkmarkIcon.svg';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-export const SigninForm = () => {
+export const SignupForm = () => {
     const [showPassword, setShowPassword] = useState(false);
     const PasswordToggleIcon = showPassword ? hidePasswordIcon : showPasswordIcon;
 
@@ -23,8 +23,25 @@ export const SigninForm = () => {
                 {logoIcon()}
             </div>
             <Stack direction="column">
-                <Text type="h2" size="xl" className={styles.heading}>Войти</Text>
-                <Text type="p" size="xs" className={styles.par}>Пожалуйста, заполните данные для доступа к вашему аккаунту.</Text>
+                <Text type="h2" size="xl" className={styles.heading}>Регистрация</Text>
+                <Text type="p" size="xs" className={styles.par}>Заполните информацию ниже.</Text>
+
+                <Stack justify="justifyBetween" className={styles.name}>
+                    <Input
+                        label="Имя"
+                        type="text"
+                        placeholder="Введите имя"
+                        id="firstname"
+                        className={styles.firstname}
+                    />
+                    <Input
+                        label="Фамилия"
+                        type="text"
+                        placeholder="Введите фамилию"
+                        id="lastname"
+
+                    />
+                </Stack>
 
                 <Input
                     label="Электронная почта"
@@ -52,19 +69,19 @@ export const SigninForm = () => {
                     <label className={styles.checkbox}>
                         <input
                             type="checkbox"
-                            className={styles.hiddenCheckbox} 
+                            className={styles.hiddenCheckbox}
                         />
                         <span className={styles.customCheckbox}>
                             <img src={checkmarkIcon} alt="checkmark" />
-                        </span> запомнить меня
+                        </span> Согласен с
+                        <a href='/' className={styles.linkAgree}> Условиями </a> и <a href='/' className={styles.linkAgree}> Политикой Конфиденциальности </a>
                     </label>
-                    <Link to="/" className={styles.forgotPassword}>Забыли пароль?</Link>
                 </Stack>
 
-                <Button size="xs" color="primary" type="submit" className={styles.signInBtn}>Войти</Button>
+                <Button size="xs" color="primary" type="submit" className={styles.signUpBtn}>Зарегистрироваться</Button>
 
                 <Text type="p" size="xs" className={styles.account}>
-                    Нет аккаунта? <Link to="/" className={styles.signUpLink}>Зарегистрироваться</Link>
+                    Уже есть аккаунт? <Link to="/" className={styles.signInLink}>Войти</Link>
                 </Text>
             </Stack>
         </form>
