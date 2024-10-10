6dafa0063e68bab94ae9ac9a00fa1dbcd19d622e
Tamara Danilova <90773979+tamara-danilova2105@users.noreply.github.com>
2024-10-10 09:24:14 +0400

    Update helper.js

--- src/app/routes/lib/helper.js
+++ src/app/routes/lib/helper.js
@@ -1,6 +1,6 @@
 export const getRouteMain = () => '/';
 export const getRouteShop = () => '/shop';
-export const getRouteCategories = (param = '') => param ? `/categories/${param}` : '/categories';
+export const getRouteCategories = (param) => `/categories${param ? `/${param}` : ''}`;
 export const getRouteAbout = () => '/about';
 export const getRouteContact = () => '/contact';
 export const getRouteBlog = () => '/blog';
