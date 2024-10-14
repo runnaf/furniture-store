7ccfa43a91db5cb6cba204f6a5c740432c2bab65
mrsBarul <dobro.ve@mail.ru>
2024-10-14 17:00:35 +0300

    return app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import Router from "./app/routes/Router";
-import { Header } from "./entities/Header/ui/Header";
-import { Navbar } from "./widgets/Navbar/ui/Navbar/Navbar"
-import styles from "./App.module.scss"
-import { useLocation } from "react-router";
-
 
 function App() {
-
-  const locations = useLocation()
-
-  const noHeaderNavbarFooter = ['/signin', '/signup']
-
-  return(
-  <main className={styles.main}>
-    {!noHeaderNavbarFooter.includes(locations.pathname) && (
-      <>
-        <Header/>
-        <Navbar/>
-      </>
-    )}
-    <Router/>
-  </main>
-  )
+  return <Router/>
 }
 
 export default App;
--- src/pages/MainPage/ui/MainPage.js
+++ src/pages/MainPage/ui/MainPage.js
@@ -1,15 +1,19 @@
 import { NewsLetter } from "../../../feature/NewsLetters/ui/NewsLetter";
 import { FlashSale } from "../../../widgets/FlashSale/ui/FlashSale";
+import { Navbar } from '../../../widgets/Navbar/ui/Navbar/Navbar';
 import { MobileNavbar } from "../../../widgets/Navbar/ui/MobileNavbar/MobileNavbar";
 import { NewsBlogs } from "../../../widgets/NewsBlogs/ui/NewsBlogs";
 import { OurProducts } from "../../../widgets/OurProducts/ui/OurProducts";
 import { QuestionMainPage } from "../../../widgets/QuestionsMainPage/ui/QuestionMainPage";
 import { TodayDeals } from "../../../widgets/TodayDeals/ui/TodayDeals";
 import styles from './MainPage.module.scss';
+import { Header } from "../../../entities/Header/ui/Header";
 
 const MainPage = () => {
     return (
         <main className={styles.main_page}>
+            <Header/>
+            <Navbar/>
             <MobileNavbar />
             <OurProducts />
             <FlashSale />
