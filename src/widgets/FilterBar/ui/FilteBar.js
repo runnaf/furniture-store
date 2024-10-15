19edc6a9ab54d531d0b3ad2b509b4a46fb365331
Daria Pavlova <dmpavlova@yandex.ru>
2024-10-15 15:49:35 +0300

    added colors to radio

--- src/widgets/FilterBar/ui/FilteBar.js
+++ src/widgets/FilterBar/ui/FilteBar.js
@@ -33,59 +33,59 @@ export function FilterBar () {
             <Text type='h2' size='s'>Настройки фильтра</Text>
             {line}
             <Stack direction='column' gap='12'>
-            <Text className={styles.subtitle}>Категория</Text>
-            <Text><input type='checkbox' value='bedroom' name='bedroom' onChange={() => handleChange('category', 'bedroom')}/> Спальня</Text>
-            <Text><input type='checkbox' value='livingRoom' name='livingRoom' onChange={() => handleChange('category', 'livingRoom')} /> Гостиная</Text>
-            <Text><input type='checkbox' value='office' name='office' onChange={() => handleChange('category', 'office')} /> Офис</Text>
-            <Text><input type='checkbox' value='lighting' name='lighting' onChange={() => handleChange('category', 'lighting')} /> Освещение</Text>
-            <Text><input type='checkbox' value='kitchen' name='kitchen' onChange={() => handleChange('category', 'kitchen')} /> Кухня</Text>
-            <Text><input type='checkbox' value='outdoor' name='outdoor' onChange={() => handleChange('category', 'outdoor')} /> Для улицы</Text>
-            <Text><input type='checkbox' value='decor' name='decor' onChange={() => handleChange('category', 'decor')} /> Декор</Text>
+                <Text className={styles.subtitle}>Категория</Text>
+                <Text><input type='checkbox' value='bedroom' name='bedroom' onChange={() => handleChange('category', 'bedroom')}/> Спальня</Text>
+                <Text><input type='checkbox' value='livingRoom' name='livingRoom' onChange={() => handleChange('category', 'livingRoom')} /> Гостиная</Text>
+                <Text><input type='checkbox' value='office' name='office' onChange={() => handleChange('category', 'office')} /> Офис</Text>
+                <Text><input type='checkbox' value='lighting' name='lighting' onChange={() => handleChange('category', 'lighting')} /> Освещение</Text>
+                <Text><input type='checkbox' value='kitchen' name='kitchen' onChange={() => handleChange('category', 'kitchen')} /> Кухня</Text>
+                <Text><input type='checkbox' value='outdoor' name='outdoor' onChange={() => handleChange('category', 'outdoor')} /> Для улицы</Text>
+                <Text><input type='checkbox' value='decor' name='decor' onChange={() => handleChange('category', 'decor')} /> Декор</Text>
             </Stack>
             {line}
             <Stack direction='column'>
-            <Text className={styles.subtitle}>Цена</Text>
-            <Stack>
-                <span className={styles.price}>{selectedFilters.price[0]}р</span>&nbsp;-&nbsp;
-                <span className={styles.price}>{selectedFilters.price[1]}р</span>
-            </Stack>
-            <ReactSlider
-                min={0}
-                max={100000}
-                step={500}
-                value={selectedFilters.price}
-                onChange={handleSliderChange}
-                renderThumb={(props) => <div {...props} className={styles.thumb} />}
-                renderTrack={(props, state) => <div {...props} className={`${styles.track} ${state.index === 0 ? styles.trackMin : styles.trackMax}`} />}
-                className={styles.slider}
-            /> 
+                <Text className={styles.subtitle}>Цена</Text>
+                <Stack>
+                    <span className={styles.price}>{selectedFilters.price[0]}р</span>&nbsp;-&nbsp;
+                    <span className={styles.price}>{selectedFilters.price[1]}р</span>
+                </Stack>
+                <ReactSlider
+                    min={0}
+                    max={100000}
+                    step={500}
+                    value={selectedFilters.price}
+                    onChange={handleSliderChange}
+                    renderThumb={(props) => <div {...props} className={styles.thumb} />}
+                    renderTrack={(props, state) => <div {...props} className={`${styles.track} ${state.index === 0 ? styles.trackMin : styles.trackMax}`} />}
+                    className={styles.slider}
+                /> 
             </Stack>           
             {line}
             <Stack direction='column' gap='12'>
-            <Text className={styles.subtitle}>Цвет</Text>
-            <Text><input type='radio' value='brown' name='brown' onChange={() => handleChange('color', 'brown')}/> Коричневый</Text>
-            <Text><input type='radio' value='grey' name='grey' onChange={() => handleChange('color', 'grey')} /> Серый</Text>
-            <Text><input type='radio' value='black' name='black' onChange={() => handleChange('color', 'black')} /> Черный</Text>
-            <Text><input type='radio' value='green' name='green' onChange={() => handleChange('color', 'green')} /> Зеленый</Text>
-            <Text><input type='radio' value='red' name='red' onChange={() => handleChange('color', 'red')} /> Красный</Text>
-            <Text><input type='radio' value='orange' name='orange' onChange={() => handleChange('color', 'orange')} /> Оранжевый</Text>
-            <Text><input type='radio' value='blue' name='blue' onChange={() => handleChange('color', 'blue')} /> Синий</Text>
-            <Text><input type='radio' value='white' name='white' onChange={() => handleChange('color', 'white')} /> Белый</Text>
+                <Text className={styles.subtitle}>Цвет</Text>
+                <Text><input className={styles.radioBrown} type='radio' value='brown' onChange={() => handleChange('color', 'brown')} /> Коричневый</Text>
+                <Text><input className={styles.radioGrey} type='radio' value='grey' onChange={() => handleChange('color', 'grey')} /> Серый</Text>
+                <Text><input className={styles.radioBlack} type='radio' value='black' onChange={() => handleChange('color', 'black')} /> Черный</Text>
+                <Text><input className={styles.radioGreen} type='radio' value='green' onChange={() => handleChange('color', 'green')} /> Зеленый</Text>
+                <Text><input className={styles.radioRed} type='radio' value='red' onChange={() => handleChange('color', 'red')} /> Красный</Text>
+                <Text><input className={styles.radioOrange} type='radio' value='orange' onChange={() => handleChange('color', 'orange')} /> Оранжевый</Text>
+                <Text><input className={styles.radioBlue} type='radio' value='blue' onChange={() => handleChange('color', 'blue')} /> Синий</Text>
+                <Text><input className={styles.radioWhite} type='radio' value='white' onChange={() => handleChange('color', 'white')} /> Белый</Text>
             </Stack>
             {line}
             <Stack direction='column' gap='12'>
-            <Text className={styles.subtitle}>Материал</Text>
-            <Text><input type='checkbox' value='metal' name='metal' onChange={() => handleChange('material', 'metal')} /> Металл</Text>
-            <Text><input type='checkbox' value='wood' name='wood' onChange={() => handleChange('material', 'wood')} /> Дерево</Text>
-            <Text><input type='checkbox' value='upholstered' name='upholstered' onChange={() => handleChange('material', 'upholstered')} /> С мягкой обивкой</Text>
-            <Text><input type='checkbox' value='glass' name='glass' onChange={() => handleChange('material', 'glass')} /> Стекло</Text>
-            <Text><input type='checkbox' value='plastic' name='plastic' onChange={() => handleChange('material', 'plastic')} /> Пластик</Text>
+                <Text className={styles.subtitle}>Материал</Text>
+                <Text><input type='checkbox' value='metal' name='metal' onChange={() => handleChange('material', 'metal')} /> Металл</Text>
+                <Text><input type='checkbox' value='wood' name='wood' onChange={() => handleChange('material', 'wood')} /> Дерево</Text>
+                <Text><input type='checkbox' value='upholstered' name='upholstered' onChange={() => handleChange('material', 'upholstered')} /> С мягкой обивкой</Text>
+                <Text><input type='checkbox' value='glass' name='glass' onChange={() => handleChange('material', 'glass')} /> Стекло</Text>
+                <Text><input type='checkbox' value='plastic' name='plastic' onChange={() => handleChange('material', 'plastic')} /> Пластик</Text>
             </Stack>
             {line}
             <Stack direction='column' gap='12'>
-            <Text className={styles.subtitle}>Доступность</Text>
-            <Text><input type='checkbox' value='inStock' name='inStock' onChange={() => handleChange('availability', 'inStock')} /> В наличии</Text>
-            <Text><input type='checkbox' value='outOfStock' name='outOfStock' onChange={() => handleChange('availability', 'outOfStock')} /> Нет в наличии</Text>
+                <Text className={styles.subtitle}>Доступность</Text>
+                <Text><input type='checkbox' value='inStock' name='inStock' onChange={() => handleChange('availability', 'inStock')} /> В наличии</Text>
+                <Text><input type='checkbox' value='outOfStock' name='outOfStock' onChange={() => handleChange('availability', 'outOfStock')} /> Нет в наличии</Text>
             </Stack>
             <Text>Выбранные фильтры: {JSON.stringify(selectedFilters)}</Text>
         </Stack>
