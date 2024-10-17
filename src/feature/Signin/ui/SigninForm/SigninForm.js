18201b04b343cb053286003cd4f4829414bfd155
mrsBarul <dobro.ve@mail.ru>
2024-10-17 08:39:55 +0300

    validate signin&signup

--- src/feature/Signin/ui/SigninForm/SigninForm.js
+++ src/feature/Signin/ui/SigninForm/SigninForm.js
@@ -3,79 +3,104 @@ import { Text } from '../../../../shared/ui/Text/Text';
 import { Input } from '../../../../shared/ui/Input/Input';
 import { Button } from '../../../../shared/ui/Button/Button';
 import { HidePasswordIcon, ShowPasswordIcon } from '../../../../shared/assets/svg/passwordIcons';
-import { LogoIcon, logoIcon } from '../../../../shared/assets/svg/navbarIcons';
+import { LogoIcon } from '../../../../shared/assets/svg/navbarIcons';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getRouteMain, getRouteSignup } from '../../../../app/routes/lib/helper';
 import { CheckmarkIcon } from '../../../../shared/assets/svg/checkmarkIcon';
 import styles from './SigninForm.module.scss';
+import { FormProvider, useForm } from 'react-hook-form';
+import { emailRegex } from '../../../../shared/libs/validation/getValidate';
+import { data } from '../../../../shared/libs/validation/errors/data'
 
 export const SigninForm = () => {
     const [showPassword, setShowPassword] = useState(false);
     const PasswordToggleIcon = showPassword ? <HidePasswordIcon /> : <ShowPasswordIcon />;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
+    const methods = useForm({mode: "onSubmit"})
+    const { register, handleSubmit, setValue, reset, formState: { errors } } = methods;
+
+    const onSubmit = () => {
+        reset()
+    }
 
     return (
-        <form className={styles.form} onSubmit={handleSubmit}>
-            <Link to={getRouteMain()}>
-                <LogoIcon />
-            </Link>
+        <FormProvider {...methods}>
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                <Link to={getRouteMain()}>
+                    <LogoIcon />
+                </Link>
 
-            <Stack direction="column" gap='32'>
-                <Text type="h2" size="xl">
-                    Войти
-                </Text>
-                <Text className={styles.par}>
-                    Заполните данные для доступа к вашему аккаунту.
-                </Text>
+                <Stack direction="column" gap='32'>
+                    <Text type="h2" size="xl">
+                        Войти
+                    </Text>
+                    <Text className={styles.par}>
+                        Заполните данные для доступа к вашему аккаунту.
+                    </Text>
 
-                <Input
-                    label="Электронная почта"
-                    type="email"
-                    placeholder="Введите адрес электронной почты"
-                    id="email"
-                />
-
-                <Stack direction='column' className={styles.password}>
                     <Input
-                        label="Пароль"
-                        type={!showPassword ? 'password' : 'text'}
-                        placeholder=" введите пароль"
-                        id="password"
+                        label="Электронная почта"
+                        placeholder="Введите адрес электронной почты"
+                        id="email"
+                        name="email"
+                        register={register}
+                        setValue={setValue}
+                        options={{
+                            required: data.required,
+                            pattern: {
+                                value: emailRegex,
+                                message: data.errors.validEmail
+                            }
+                        }}
+                        error={errors.email}
                     />
-                    <button className={styles.password_btn}
-                        onClick={() => setShowPassword(!showPassword)}>
-                        {PasswordToggleIcon}
-                    </button>
-                </Stack>
 
-                <Stack justify="justifyBetween" align="center">
-                    <label className={styles.checkbox}>
-                        <input
-                            type="checkbox"
-                            className={styles.hiddenCheckbox}
+                    <Stack direction='column' className={styles.password}>
+                        <Input
+                            label="Пароль"
+                            type={!showPassword ? 'password' : 'text'}
+                            placeholder=" введите пароль"
+                            id="password"
+                            name='password'
+                            register={register}
+                            setValue={setValue}
+                            options={{
+                                required: data.required,
+                            }}
+                            error={errors.password}
                         />
-                        <span className={styles.customCheckbox}>
-                            <CheckmarkIcon />
-                        </span> запомнить меня
-                    </label>
-                    
-                    {/* TODO */}
-                    <Link to="/" className={styles.forgotPassword}>Забыли пароль?</Link>
+                        <button className={styles.password_btn}
+                            onClick={() => setShowPassword(!showPassword)}>
+                            {PasswordToggleIcon}
+                        </button>
+                    </Stack>
+
+                    <Stack justify="justifyBetween" align="center">
+                        <label className={styles.checkbox}>
+                            <input
+                                type="checkbox"
+                                className={styles.hiddenCheckbox}
+                            />
+                            <span className={styles.customCheckbox}>
+                                <CheckmarkIcon />
+                            </span> запомнить меня
+                        </label>
+                        
+                        {/* TODO */}
+                        <Link to="/" className={styles.forgotPassword}>Забыли пароль?</Link>
+                    </Stack>
+
+                    <Button type="submit" className={styles.signInBtn}>
+                        Войти
+                    </Button>
+
+                    <Text className={styles.account}>
+                        Нет аккаунта? 
+                        <Link to={getRouteSignup()} className={styles.signUpLink}>Зарегистрироваться</Link>
+                    </Text>
                 </Stack>
-
-                <Button type="submit" className={styles.signInBtn}>
-                    Войти
-                </Button>
-
-                <Text className={styles.account}>
-                    Нет аккаунта? 
-                    <Link to={getRouteSignup()} className={styles.signUpLink}>Зарегистрироваться</Link>
-                </Text>
-            </Stack>
-        </form>
+            </form>
+        </FormProvider>
     );
 };
--- src/shared/libs/validation/errors/data.js
+++ src/shared/libs/validation/errors/data.js
@@ -4,6 +4,10 @@ export const data = {
         validName: "Используйте только буквы",
         validEmail: "Введите корректный адрес почты",
         validPhone: "Введите корректный номер телефона",
-        validNumbers: "Используйте только цифры"
+        validNumbers: "Используйте только цифры",
+        passwordTooShort: "Минимум 8 символов",
+        passwordMissingLowercase: "Хотя бы одна строчная буква",
+        passwordMissingUppercase: "Хотя бы одна заглавная буква",
+        passwordMissingDigit: "Хотя бы одна цифра"
     }
 }
