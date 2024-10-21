d7837c58e2a8a0e35046ffb4d54fbe81d7b0156e
tamara-danilova2105 <sap1028@mail.ru>
2024-10-21 21:39:45 +0400

    section style

--- src/widgets/OurProducts/ui/OurProducts.js
+++ src/widgets/OurProducts/ui/OurProducts.js
@@ -8,32 +8,34 @@ import { HeaderSection } from '../../../shared/ui/HeaderSection/HeaderSection';
 export function OurProducts () {
     return (
         <Stack className={styles.parenContainer}>
-        <Stack 
-            direction='column' 
-            align='alignCenter' 
-            gap='16' 
-            className={styles.container}
-        >
-            <HeaderSection subTitle='Наша продукция'>
-                <span>Каталог товаров</span>
-            </HeaderSection>
-
-            <Stack gap='16' className={styles.buttonsContainer}>
-                <Button size='s' color='outlined'>Все товары</Button>
-                <Button size='s'>Новинки</Button>
-                <Button size='s' color='outlined'>Популярное</Button>
-                <Button size='s' color='outlined'>Рекомендуемые</Button>
-            </Stack>
             <Stack 
-                justify='justifyBetween'
-                className={styles.cardContainer}
-                gap='32'
+                direction='column' 
+                align='alignCenter' 
+                gap='16' 
+                className={styles.container}
             >
-                {cardData.map((element) => (
-                    <div className={styles.cardWrapper} key={element.id}><Card {...element} /></div>
-                ))}
+                <HeaderSection subTitle='Наша продукция'>
+                    <span>Каталог товаров</span>
+                </HeaderSection>
+
+                <Stack gap='16' className={styles.buttonsContainer}>
+                    <Button color='outlined'>Все товары</Button>
+                    <Button >Новинки</Button>
+                    <Button color='outlined'>Популярное</Button>
+                    <Button color='outlined'>Рекомендуемые</Button>
+                </Stack>
+                <Stack 
+                    justify='justifyBetween'
+                    className={styles.cardContainer}
+                    gap='32'
+                >
+                    {cardData.map((element) => (
+                        <div className={styles.cardWrapper} key={element.id}>
+                            <Card {...element} />
+                        </div>
+                    ))}
+                </Stack>
             </Stack>
-        </Stack>
         </Stack>
     )
 }
