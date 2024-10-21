74cb7a0f90e4a5a0699cac5fade3e6b417176457
“Irina <runnaf@mail.ru>
2024-10-21 21:03:40 +0300

    fixed

--- src/entities/Slider/ui/BottomButtons/BottomButtons.js
+++ src/entities/Slider/ui/BottomButtons/BottomButtons.js
@@ -1,20 +1,12 @@
-import { getStyles } from "../../../../shared/libs/getStyles";
 import { Button } from "../../../../shared/ui/Button/Button";
 import styles from "./BottomButtons.module.scss"
 
-export const BottomButtons = ({styleMode, quantitySliders, quantityCardsOnPage, currentSlide, handleClickSlide}) => {
+export const BottomButtons = ({quantitySliders, quantityCardsOnPage, currentSlide, handleClickSlide}) => {
     const lengthArray = Math.ceil(quantitySliders.length / quantityCardsOnPage);
     const array = Array.from({length: lengthArray}, (_, i) => i);
     
-    const mode = { [styleMode]: true }
-
-    const additional = [
-        styles[styleMode]
-    ]; 
-    const styleClass = getStyles(styles.container, mode, additional);
-    
     return (
-        <ul className = {styleClass}>
+        <ul className = {styles.container}>
             {array.map((page) => 
                 <li key={page} onClick = {()=> handleClickSlide(page)}>
                     <Button id={page} className = {currentSlide === page ? '' : styles.notPressed} />
