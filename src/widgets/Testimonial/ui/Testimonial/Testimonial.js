8575585b0162c22029d8dc31c2527db80e6d86f1
tamara-danilova2105 <sap1028@mail.ru>
2024-10-23 10:36:36 +0400

    upd testimonials

--- src/widgets/Testimonial/ui/Testimonial/Testimonial.js
+++ src/widgets/Testimonial/ui/Testimonial/Testimonial.js
@@ -1,15 +1,15 @@
 import { Slider } from "../../../../entities/Slider/ui/Slider/Slider";
 import { HeaderSection } from "../../../../shared/ui/HeaderSection/HeaderSection";
-import { QUANTITY_CARD_ON_PAGE, testimonial, WIDTH_MOBILE_XL } from "../../lib/data"
-import styles from "./Testimonial.module.scss";
+import { QUANTITY_CARD_ON_PAGE, testimonial, WIDTH_MOBILE_XL } from "../../lib/data";
 import { useSlider } from "../../../../entities/Slider/hooks/useSlider";
 import { slicerOfArray } from "../../../../entities/Slider/lib/helper";
 import { Reviews } from "../Reviews/Reviews";
 import { useResize } from "../../../../shared/hooks/useResize";
-
+import styles from "./Testimonial.module.scss";
+import { Stack } from "../../../../shared/ui/Stack/Stack";
 
 export const Testimonial = () => {
-    const { currentSlide, nextCard,  prevCard, handleClickSlide } = useSlider(testimonial.length);
+    const { currentSlide, nextCard, prevCard, handleClickSlide } = useSlider(testimonial.length);
     const width = useResize();
 
     const quantityCardsOnPage = () => {        
@@ -21,21 +21,28 @@ export const Testimonial = () => {
     const reviews = slicerOfArray(testimonial, currentSlide, quantityCardsOnPage());
 
     return (
-        <section className={ styles.testimonialContainer }>
-            <HeaderSection subTitle= "Отзывы">Что <span>Говорят наши клиенты</span></HeaderSection>
-            <Slider isSideButtons = {false} 
-                    isBottomButtons = {true} 
-                    quantityCardsOnPage = { quantityCardsOnPage() } 
-                    className = {styles.slider} 
-                    data = {testimonial} 
-                    currentSlide = {currentSlide} 
-                    nextCard = { nextCard } 
-                    prevCard = { prevCard }
-                    handleClickSlide = { handleClickSlide }
-                    gap="32">
-                    <Reviews reviews = {reviews}/>
-            </Slider>
+        <section className={styles.testimonialContainer}>
+            <HeaderSection subTitle= "Отзывы">
+                Что <span>Говорят наши клиенты</span>
+            </HeaderSection>
+            <Stack 
+                className={styles.testimonialsWrapper}
+            >
+                <Slider 
+                    isSideButtons={false} 
+                    isBottomButtons={true} 
+                    quantityCardsOnPage={quantityCardsOnPage()} 
+                    className={styles.slider} 
+                    data={testimonial} 
+                    currentSlide={currentSlide} 
+                    nextCard={nextCard} 
+                    prevCard={prevCard}
+                    handleClickSlide={handleClickSlide}
+                    gap="32"
+                >
+                    <Reviews reviews={reviews}/>
+                </Slider>
+            </Stack>
         </section>
-        
-    )
-}
+    );
+};
