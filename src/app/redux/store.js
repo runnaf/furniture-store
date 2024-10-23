a3a2165e2b50a71d3ae1c6665126a321ed63c09b
mrsBarul <dobro.ve@mail.ru>
2024-10-23 17:22:38 +0300

    uncommented store

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,14 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { blogApi } from "../../widgets/NewsBlogs/api/blogApi";
 
-// const rootReducer = combineReducers({
-//     [blogApi.reducerPath]: blogApi.reducer,
-//     // по аналогии следующие api-сервисы,
-// })
+const rootReducer = combineReducers({
+    [blogApi.reducerPath]: blogApi.reducer,
+    // по аналогии следующие api-сервисы,
+})
 
 export const store = configureStore({
-    reducer: {}
-    
-    // rootReducer,
-    // middleware: (getDefaultMiddleware) => 
-    //     getDefaultMiddleware().concat(blogApi.middleware)
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(blogApi.middleware)
 })
