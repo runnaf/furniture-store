c4284fdaf360115580f0106a7288c484a29d7f4e
“Irina <runnaf@mail.ru>
2024-10-28 18:23:34 +0300

    fixed bottomButtons component

---
+++ src/entities/Slider/ui/BottomButtons/ui/BottomButtons/BottomButtons.js
@@ -0,0 +1,22 @@
+import { BottomButton } from "../BottomButton/BottomButton";
+import styles from "./BottomButtons.module.scss"
+
+export const BottomButtons = ({
+        quantitySliders, 
+        quantityCardsOnPage, 
+        currentSlide, 
+        handleClickSlide,
+        color = 'primary' //primary | secondary
+    }) => {
+    const lengthArray = Math.ceil(quantitySliders.length / quantityCardsOnPage);
+    
+    const array = Array.from({length: lengthArray}, (_, i) => i);
+    console.log(array)
+    return (
+        <ul className = {styles.container}>
+            {array.map((page) => 
+                <BottomButton key={page} page={page} color={color} currentSlide={currentSlide} handleClickSlide={handleClickSlide} />
+            )}
+        </ul>
+    )
+}
