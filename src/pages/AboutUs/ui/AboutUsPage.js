fa65a450f94d4e193f8da1c30cac215b3a4881da
“Irina <runnaf@mail.ru>
2024-11-11 18:05:14 +0300

    fixed div tag on main in pages

--- src/pages/AboutUs/ui/AboutUsPage.js
+++ src/pages/AboutUs/ui/AboutUsPage.js
@@ -1,6 +1,5 @@
 import { Breadcrumbs } from "../../../entities/BreadCrumbs/ui/BreadCrumbs";
 import { SectionTitle } from "../../../entities/SectionTitle/ui/SectionTitle";
-import { Stack } from "../../../shared/ui/Stack/Stack";
 import { routes } from "../../../app/routes/lib/data";
 import { Advantages } from "../../../entities/Advantages/ui/Advantages/Advantages";
 import { ProductQuality } from "../../../widgets/ProductQuality/ui/ProductQuantity/ProductQuality";
@@ -9,18 +8,13 @@ import styles from "./AboutUsPage.module.scss";
 
 export const AboutUsPage = () => {
     return (
-        <Stack 
-            direction="column" 
-            justify='justifyCenter' 
-            align='alignCenter' 
-            gap='75' className={styles.container}
-        >
+        <main className={styles.container}>
             <SectionTitle>
                 <Breadcrumbs routes={routes}/>
             </SectionTitle>
             <AboutUsPreview />
             <ProductQuality />
             <Advantages />
-        </Stack>
+        </main>
     )
 }
--- src/pages/ProductPage/ui/ProductPage.js
+++ src/pages/ProductPage/ui/ProductPage.js
@@ -1,16 +1,15 @@
 import { Breadcrumbs } from "../../../entities/BreadCrumbs/ui/BreadCrumbs";
 import { SectionTitle } from "../../../entities/SectionTitle/ui/SectionTitle";
-import { Stack } from "../../../shared/ui/Stack/Stack";
 import { routes } from "../../../app/routes/lib/data";
-import styles from "./ProductPage.module.scss";
 import { ProductPreview } from "../../../widgets/ProductPreview/ui/ProductPreview/ProductPreview";
 import { Advantages } from "../../../entities/Advantages/ui/Advantages/Advantages";
 import { RelatedProducts } from "../../../widgets/RelatedProducts/RelatedProducts";
 import { Tabs } from "../../../widgets/TabsProduct/ui/Tabs/Tabs";
+import styles from "./ProductPage.module.scss";
 
 export const ProductPage = () => {
     return (
-        <Stack direction="column" justify='justifyCenter' align='alignCenter' gap='75' className={styles.container}>
+        <main className={styles.container}>
             <SectionTitle>
                 <Breadcrumbs routes={routes}/>
             </SectionTitle>
@@ -18,6 +17,6 @@ export const ProductPage = () => {
             <Tabs />
             <RelatedProducts />
             <Advantages />
-        </Stack>
+        </main>
     )
 }
