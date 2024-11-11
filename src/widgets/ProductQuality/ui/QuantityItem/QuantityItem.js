0ff9192d9e6e14a7d8e2a5d54a12f1e16e3e87a0
“Irina <runnaf@mail.ru>
2024-11-11 13:01:25 +0300

    fixed adaptation AboutUs page

--- src/widgets/ProductQuality/ui/QuantityItem/QuantityItem.js
+++ src/widgets/ProductQuality/ui/QuantityItem/QuantityItem.js
@@ -1,9 +1,10 @@
-import { Stack } from "../../../../shared/ui/Stack/Stack"
-import { Text } from "../../../../shared/ui/Text/Text"
+import { Stack } from "../../../../shared/ui/Stack/Stack";
+import { Text } from "../../../../shared/ui/Text/Text";
+import styles from "./QuantityItems.module.scss";
 
 export const QuantityItem = ({item}) => {
     return (
-        <Stack direction="column" gap="16">
+        <Stack className={styles.container} direction="column" gap="16">
             <img src={item.img} alt="Изображение отражающее качество" width="60" height="60"/>
             <Text size="s">{item.title}</Text>
             <Text>{item.text}</Text>
