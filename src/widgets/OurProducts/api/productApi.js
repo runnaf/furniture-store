a1f211003fa62f726e7efd5d9cc7fc820dc04566
Богдан Ишмаев <bodyaev@MacBook-Pro-Bogdan.local>
2024-11-12 08:29:28 +0300

    created productApi

---
+++ src/widgets/OurProducts/api/productApi.js
@@ -0,0 +1,58 @@
+import { api } from "../../../shared/api/api";
+import { endpoints } from "../../../shared/api/endpoints";
+import { createApiConfig } from "../../../shared/api/helpers";
+
+
+const PRODUCT_TAG = [{ type: 'Product', id: 'Product'}];
+
+
+const productApi = api.injectEndpoints({
+    endpoints:(build) => ({
+        getAllProducts: build.query({
+            query: ({ params }) => createApiConfig(
+                'GET', 
+                `${endpoints.lists.goods}?${new URLSearchParams(params).toString()}`
+            ),
+            providesTags: () => PRODUCT_TAG,
+        }),
+        getProduct: build.query({
+            query: ({ id }) => createApiConfig(
+                'GET',
+                `${endpoints.lists.goods}/${id}`
+            ),
+            providesTags: () => PRODUCT_TAG,
+        }),
+        addProduct: build.mutation({
+            query: (body) => createApiConfig(
+                'POST',
+                endpoints.lists.goods,
+                body
+            ),
+            invalidatesTags: PRODUCT_TAG,
+        }),
+        editProduct: build.mutation({
+            query: (body) => createApiConfig(
+                'PUT',
+                `${endpoints.lists.goods}/${body.id}`,
+                body
+            ),
+            invalidatesTags: PRODUCT_TAG,
+        }),
+        deleteProduct: build.mutation({
+            query: ({ id }) => createApiConfig(
+                'DELETE',
+                `${endpoints.lists.goods}/${id}`
+            ),
+            invalidatesTags: PRODUCT_TAG,
+        })
+    })
+})
+
+export const {
+    useGetProductQuery,
+    useGetAllProductsQuery,
+    useLazyGetAllProductsQuery,
+    useAddProductMutation,
+    useEditProductMutation,
+    useDeleteProductMutation
+} = productApi
