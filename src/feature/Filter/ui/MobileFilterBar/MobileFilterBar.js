367ebbc1f6837c79584d4448f7646b1532978ced
Daria Pavlova <dmpavlova@yandex.ru>
2024-11-14 15:48:49 +0300

    fixes

--- src/feature/Filter/ui/MobileFilterBar/MobileFilterBar.js
+++ src/feature/Filter/ui/MobileFilterBar/MobileFilterBar.js
@@ -1,4 +1,4 @@
-import styles from './MobileFilterBar.module.scss'; 
+import React, { useEffect, useState } from 'react';
 import filter from '../../../../shared/assets/svg/filter.svg';
 import { Stack } from '../../../../shared/ui/Stack/Stack';
 import { FilterBar } from '../FilterBar/FilterBar';
@@ -6,13 +6,29 @@ import { Button } from '../../../../shared/ui/Button/Button';
 import { useToggleMenu } from '../../../../shared/hooks/useToggleMenu';
 import useOverflowHidden from '../../../../shared/hooks/useOverflowHidden';
 import { DeleteFilter } from '../../../../shared/assets/svg/deleteFilter';
+import styles from './MobileFilterBar.module.scss'; 
 
 export function MobileFilterBar() {
 
     const { isOpen, toggleMenu, menuRef } = useToggleMenu();
+    const [isVisible, setVisible] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+            const timer = setTimeout(() => {
+                setVisible(true); 
+            }, 300); 
+            return () => clearTimeout(timer);
+        } else {
+            const timer = setTimeout(() => {
+                setVisible(false);
+            }, 500); 
+            return () => clearTimeout(timer);
+        }
+    }, [isOpen]);
 
     useOverflowHidden(isOpen);
-
+    
     return (
         <Stack>
             <Button color='outlined' onClick={toggleMenu} className={styles.filterButton}>
@@ -20,22 +36,23 @@ export function MobileFilterBar() {
                 Фильтр
             </Button>
 
-            {isOpen && (
-            <Stack 
-                ref={menuRef} 
-                justify='justifyCenter'
-                className={styles.popup}
-            >
+            {isVisible && (
                 <Stack 
-                    className={styles.popupContent} 
-                    direction='column' 
-                    gap='12'
+                    ref={menuRef} 
+                    justify='justifyCenter'
+                    className={`${styles.popup} ${isOpen ? styles.open : ''}`}
                 >
-                    <Button onClick={toggleMenu}>
-                        <DeleteFilter/></Button>
-                    <FilterBar toggleMenu={toggleMenu}/>
+                    <Stack 
+                        className={styles.popupContent} 
+                        direction='column' 
+                        gap='12'
+                    >
+                        <Button onClick={toggleMenu}>
+                            <DeleteFilter/>
+                        </Button>
+                        <FilterBar toggleMenu={toggleMenu}/>
+                    </Stack>
                 </Stack>
-            </Stack>
             )}
         </Stack>
     )
