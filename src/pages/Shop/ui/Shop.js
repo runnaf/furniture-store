27865f42f16e3c5f3e23375c50b2dc335917a20d
Богдан Ишмаев <bodyaev@MacBook-Pro-Bogdan.local>
2024-12-05 09:39:18 +0300

    rename

--- src/pages/Shop/ui/Shop.js
+++ src/pages/Shop/ui/Shop.js
@@ -14,9 +14,6 @@ const Shop = () => {
     const width = useResize();
     const isMobile = width <= 820;
 
-    const token = Cookies.get('authToken')
-    console.log(token)
-
     return (
         <main 
             className={styles.container}
